e1b89ad5c045c88e3385a5afba4b04601a61ae73
shiruiqiang <shiruiqiang@baidu.com>
2023-07-04 19:09:18 +0800

    add normalize.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 /*
  * @Author: shiruiqiang
  * @Date: 2023-07-03 11:19:58
- * @LastEditTime: 2023-07-04 16:25:34
+ * @LastEditTime: 2023-07-04 19:08:33
  * @LastEditors: shiruiqiang
  * @FilePath: App.tsx
  * @Description: shiruiqiang
  */
 import React from 'react';
+import 'normalize.css';
 import './styles/index.scss';
 
 const RootComponent = React.lazy(() => import('./components/button/demo/base-button'));
