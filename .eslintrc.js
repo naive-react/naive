9d09a91b3778f73fda02457e316d18520f75e87f
shiruiqiang <shiruiqiang@baidu.com>
2023-07-04 16:07:22 +0800

    add eslint and pre-commit

---
+++ .eslintrc.js
@@ -0,0 +1,60 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "standard-with-typescript",
+        "plugin:react/recommended"
+    ],
+    "overrides": [
+        {
+            "files": ["src/*.ts", "src/*.tsx"],
+            "rules": {
+                "@typescript-eslint/no-unused-vars": [
+                    2,
+                    {
+                        "args": "none"
+                    }
+                ]
+            }
+        },
+        {
+            "files": ["*.js"],
+            "rules": {
+                "@typescript-eslint/no-var-requires": 0
+            }
+        }
+    ],
+    "parserOptions": {
+        sourceType: "module",
+        ecmaFeatures: {
+            "jsx": true
+        },
+        parser: "@typescript-eslint/parser",
+        project: "./tsconfig.json",
+        tsconfigRootDir: __dirname,
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        'no-unused-vars': 'warn',
+        'no-undef': 'warn',
+        'no-labels': 'off',
+        'camelcase': 'off',
+        "semi": ["error", "always"],
+        "@typescript-eslint/semi": 'off',
+        "block-spacing": ["error", "never"],
+        "@typescript-eslint/object-curly-spacing": ["error", "always"],
+        "@typescript-eslint/consistent-type-imports": "off",
+        "@typescript-eslint/explicit-function-return-type": "off",
+        "react/display-name": "off",
+        "react/react-in-jsx-scope": "off"
+    },
+    "settings": {
+        react: {
+            version: "detect",
+        },
+    },
+}
---
+++ craco.config.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: shiruiqiang
+ * @Date: 2023-07-04 15:40:20
+ * @LastEditTime: 2023-07-04 15:40:36
+ * @LastEditors: shiruiqiang
+ * @FilePath: craco.config.js
+ * @Description: shiruiqiang
+ */
+module.exports = {
+    babel: {
+        plugins: ['react-require'],
+    },
+}
