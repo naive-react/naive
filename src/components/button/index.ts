3934bb84758ba5484ce9737a98822a1cedc7d931
shiruiqiang <shiruiqiang@baidu.com>
2023-07-04 16:10:54 +0800

    add button export entry

---
+++ src/components/button/index.ts
@@ -0,0 +1,10 @@
+/*
+ * @Author: shiruiqiang
+ * @Date: 2023-07-04 14:38:59
+ * @LastEditTime: 2023-07-04 16:09:40
+ * @LastEditors: shiruiqiang
+ * @FilePath: index.ts
+ * @Description: shiruiqiang
+ */
+export * as Button from './src/button';
+export type { ButtonProps } from './src/types';
