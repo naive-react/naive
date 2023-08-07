23872fc9112f7a9fcabab20ce1b80a7809dbc404
shiruiqiang <shiruiqiang@baidu.com>
2023-08-07 14:45:44 +0800

    fixbug(button-demo): fix button-demo group-button icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: shiruiqiang
  * @Date: 2023-07-03 11:19:58
- * @LastEditTime: 2023-07-17 17:29:06
+ * @LastEditTime: 2023-08-07 14:29:30
  * @LastEditors: shiruiqiang
  * @FilePath: App.tsx
  * @Description: shiruiqiang
@@ -10,9 +10,9 @@ import React from 'react';
 import 'normalize.css';
 import './styles/index.scss';
 
-// const RootComponent = React.lazy(() => import('./components/button/demo/base-button'));
+const RootComponent = React.lazy(() => import('./components/button/demo/base-button'));
 // const RootComponent = React.lazy(() => import('./components/icon/demo/base-icon'));
-const RootComponent = React.lazy(() => import('./components/avatar/demo/base-avatar'));
+// const RootComponent = React.lazy(() => import('./components/avatar/demo/base-avatar'));
 
 function App () {
     return (
