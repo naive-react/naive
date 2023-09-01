c77ddf00719be29f6b45a3a858a4e860df2daec0
shiruiqiang <shiruiqiang@baidu.com>
2023-09-01 16:54:20 +0800

    update(card): add card unit-test

--- src/components/card/src/card.spec.tsx
+++ src/components/card/src/card.spec.tsx
@@ -1,17 +1,76 @@
+import {fireEvent, render, renderHook} from '@testing-library/react';
+import {Card} from './card';
+import {useState} from 'react';
+import {act} from 'react-dom/test-utils';
+import {CardSize} from './type';
+
 /*
  * @Author: shiruiqiang
  * @Date: 2023-08-08 15:04:11
- * @LastEditTime: 2023-08-08 22:55:19
+ * @LastEditTime: 2023-09-01 16:51:09
  * @LastEditors: shiruiqiang
  * @FilePath: card.spec.tsx
  * @Description: shiruiqiang
  */
-import {render, fireEvent, waitFor} from '@testing-library/react-native'; import React from 'react'; import {CustomTag} from '@ui/NativeTags'; import {Card} from './Card';
-
-describe('Card', () => {
-    it('matches snapshot with default props', () => {const {getByTestId} = render(<Card data-testid="Card" />); expect(getByTestId('Card')).toMatchSnapshot();});
-
-    it('matches snapshot with custom props', () => {const {getByTestId} = render(<Card data-testid="Card" size="large" bordered={false} closable embedded hoverable tag="article" title="Card Title" className="custom-class" style={{backgroundColor: 'red'}} cover={<div>Cover Content</div>} header={<div>Header Content</div>} headerExtra={<div>Header Extra Content</div>} footer={<div>Footer Content</div>} action={<div>Action Content</div>} />); expect(getByTestId('Card')).toMatchSnapshot();});
-
-    it('updates state when close button is clicked', async () => {const onClose = jest.fn(); const {getByTestId} = render(<Card data-testid="Card" onClose={onClose}> <div>Card Content</div> </Card>); const closeButton = getByTestId('Card-closeButton'); await fireEvent(closeButton, new Event('click')); expect(onClose).toHaveBeenCalled();});
+describe('card component', () => {
+    it('common card', () => {
+        const CommonCard = render(<Card>测试 card</Card>);
+        const wrapper = CommonCard.container.getElementsByClassName('n-card')[0];
+        expect(wrapper).toBeInTheDocument();
+    });
+    it('cover card', () => {
+        const CoverCard = render(<Card title='带封面的卡片' cover={<img src="https://07akioni.oss-cn-beijing.aliyuncs.com/07akioni.jpeg"></img>}>卡片内容</Card>);
+        const wrapper = CoverCard.container.getElementsByClassName('n-card-cover')[0];
+        expect(wrapper).toBeInTheDocument();
+    });
+    it('slot card', () => {
+        const SlotCard = render(<Card
+            title='卡片插槽示例'
+            headerExtra='#headerExtra'
+            footer='#footer'
+            action='#action'
+        >
+            卡片内容
+        </Card>);
+        const footerWrapper = SlotCard.container.getElementsByClassName('n-card-footer-medium')[0];
+        expect(footerWrapper).toBeInTheDocument();
+        const actionWrapper = SlotCard.container.getElementsByClassName('n-card-action-medium')[0];
+        expect(actionWrapper).toBeInTheDocument();
+    });
+    it('close card', () => {
+        const {result} = renderHook(() => useState(true));
+        const TestComponent = () => <>
+            {result.current[0] && <Card
+                closable
+                title='卡片'
+                onClose={() => result.current[1](false)}
+            >
+                卡片内容
+            </Card>}
+        </>;
+        const CloseCard = render(<TestComponent/>);
+        const closeWrapper = CloseCard.container.getElementsByClassName('n-card-header-close')[0];
+        act(() => {
+            fireEvent.click(closeWrapper);
+        });
+        CloseCard.rerender(<TestComponent/>);
+        const hidenWrapper = CloseCard.container.getElementsByClassName('n-card')[0];
+        expect(hidenWrapper).toEqual(undefined);
+    });
+    it('size card', () => {
+        const size = ['small', 'large', 'huge'];
+        size.forEach(s => {
+            const sizeCard = render(<Card size={s as CardSize}>卡片内容</Card>);
+            const sizeWrapper = sizeCard.container.getElementsByClassName(`n-card-content-${s}`)[0];
+            expect(sizeWrapper).toBeInTheDocument();
+        });
+    });
+    it('noborder card', () => {
+        const NoBorderCard = render(<Card bordered={false} title=' 无边框的卡片'>卡片内容</Card>);
+        expect(NoBorderCard.container.querySelector('.n-card')).not.toHaveClass('n-card-border');
+    });
+    it('embedded', () => {
+        const EmbeddendCard = render(<Card embedded title='📖 如何成功' bordered={false}>卡片内容</Card>);
+        expect(EmbeddendCard.container.querySelector('.n-card')).toHaveClass('n-card-embedded');
+    });
 });
